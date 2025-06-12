0f4936e0f569a608cbfec96f74147ade63e13b56
Aivanii <aivanellok@gmail.com>
2025-06-12 10:55:41 +0700

    deleted useless files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Main page - Punk Forum" },
+    { name: "description", content: "Welcome to Russian Punk Forum!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>home</>;
 }
