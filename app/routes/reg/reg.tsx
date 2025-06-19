701dec6a71f5359bb50392acc15bb43106cea314
Aivanii <aivanellok@gmail.com>
2025-06-19 09:41:27 +0700

    added displaying error message in register for the future

--- app/routes/reg/reg.tsx
+++ app/routes/reg/reg.tsx
@@ -19,12 +19,43 @@ export default function Reg() {
     passAgain: "",
   });
 
+  interface statusMsg {
+    status: "success" | "fail" | null;
+    msg: string;
+  }
+
+  const [statusMsg, setStatusMsg] = useState<statusMsg>({
+    status: null,
+    msg: "",
+  });
+
   return (
     <div className="bg-[#192024] w-dvw h-dvh">
       <form
         onSubmit={handleSubmit}
         className="absolute top-1/2 left-1/2 -translate-1/2 flex justify-center items-center flex-col gap-4"
       >
+        <div className="flex gap-4 bg-[var(--secondary-color)] w-full p-4 rounded-xl">
+          {statusMsg.status === "success" && (
+            <img
+              alt="Success"
+              width="48"
+              height="48"
+              src="https://www.reshot.com/preview-assets/icons/AEJ9U3XCGB/success-AEJ9U3XCGB.svg"
+            />
+          )}
+          {statusMsg.status === "fail" && (
+            <img
+              alt="Error"
+              width="48"
+              height="48"
+              src="https://www.reshot.com/preview-assets/icons/CLWP24AVMS/error-CLWP24AVMS.svg"
+            />
+          )}
+
+          {statusMsg.msg && <div> statusMsg.msg</div>}
+        </div>
+
         <h1 className="main-title">Регистрация</h1>
         <div className="flex flex-col">
           <label htmlFor="email">Почта:</label>
