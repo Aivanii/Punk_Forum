97d08a5569f9b93360098b76875967d563f0e21a
Aivanii <aivanellok@gmail.com>
2025-06-19 09:14:20 +0700

    added visual detection of valid form fields

--- app/routes/reg/checkFormFields.ts
+++ app/routes/reg/checkFormFields.ts
@@ -1,9 +1,17 @@
 const checkFormEmail = (email: string): boolean => {
-  return false;
+  const EMAIL_REGEXP =
+    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+  return EMAIL_REGEXP.test(email);
 };
 
 const checkFormPasswords = (pass: string, passAgain: string): boolean => {
-  return false;
+  const MIN_PASS_LENGTH: number = 8;
+  if (pass.length < MIN_PASS_LENGTH) {
+    return false;
+  }
+
+  return pass === passAgain;
 };
 
 export { checkFormEmail, checkFormPasswords };
