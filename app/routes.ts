d6d704cb811e25620c4a471ddce628e196fe2ee0
Aivanii <aivanellok@gmail.com>
2025-06-25 17:56:10 +0700

    added user agreement page

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,13 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
+  //main
   index("routes/home.tsx"),
+  //valid
   route("/reg", "./routes/reg/reg.tsx"),
   route("/login", "./routes/login/login.tsx"),
+  //laws
+  route("/user_agreement", "./routes/userAgreement/userAgreement.tsx"),
+  //other
   route("/test", "./routes/test.tsx"),
 ] satisfies RouteConfig;
